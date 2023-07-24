25e670ca7bcb0377a7157ad80ed56ae8d5f6a8a9
RamonJacques <ramonjacquesrs@gmail.com>
2023-07-24 14:24:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/exercise/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.exercise.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exercise.course.entities.User;
+import com.exercise.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
