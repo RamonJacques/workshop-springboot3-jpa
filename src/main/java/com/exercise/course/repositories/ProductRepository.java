dae6604d7feb83b620f17484f419e1492a708566
RamonJacques <ramonjacquesrs@gmail.com>
2023-07-25 19:53:20 -0300

    product

---
+++ src/main/java/com/exercise/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.exercise.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.exercise.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
