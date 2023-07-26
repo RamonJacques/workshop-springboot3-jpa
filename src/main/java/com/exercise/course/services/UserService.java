c765605bcbe12340058fcfce705a06ae32b6dfd4
RamonJacques <ramonjacquesrs@gmail.com>
2023-07-26 15:59:50 -0300

    User update

--- src/main/java/com/exercise/course/services/UserService.java
+++ src/main/java/com/exercise/course/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long Id, User obj) {
+		User entity = repository.getReferenceById(Id);
+		updateData(entity, obj);
+		return repository.save(entity);				
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());		
+	}
 }
